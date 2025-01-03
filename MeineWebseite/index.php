131d39986d05f3e524b37405f04e902e29d227d7
DerBESTEBen <ben150905@gmail.com>
2025-01-03 12:14:46 +0100

    Add files via upload

---
+++ MeineWebseite/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spielmenü</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="menu-container">
+        <h1>Spielmenü</h1>
+        <ul class="menu-list">
+            <li><a href="/PHP/spiel.php">Spielen</a></li>
+            <li><a href="/PHP/spielandleitung.php">Spielanleitung</a></li>
+            <li><a href="/PHP/einstellungen.php">Einstellungen</a></li>
+            <li><a href="/PHP/beenden.php">Beenden</a></li>
+        </ul>
+    </div>
+</body>
+</html>
