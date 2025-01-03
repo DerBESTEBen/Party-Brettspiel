3dd060b9461e9e45c034ad205589b5e13bb73dad
DerBESTEBen <ben150905@gmail.com>
2025-01-03 12:13:42 +0100

    Add files via upload

---
+++ MeineWebseite/MeineWebseite/PHP/spielandleitung.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spielanleitung</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="instructions-container">
+        <h1>Spielanleitung</h1>
+        <h2>Spielablauf:</h2>
+        <ol>
+            <li>Einstellen, wie viele Spieler mitspielen.</li>
+            <li>Eingabe der Namen.</li>
+            <li>Spielstart.</li>
+            <li>Erster Spieler würfelt.</li>
+            <li>Spielfigur des Spielers zieht die gewürfelte Augenzahl.</li>
+            <li>Aktion des aktuellen Spielfeldes wird ausgeführt (Vor, zurück, Joker).</li>
+            <li>Ranglisten werden aktualisiert.</li>
+            <li>Spielzug wird beendet.</li>
+            <li>Nächster Spieler würfelt.</li>
+        </ol>
+        <a href="../index.php" class="btn">Zurück zum Hauptmenü</a>
+    </div>
+</body>
+</html>
