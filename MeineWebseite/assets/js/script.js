1c14bfae33a87507effbcc7a5cdd482e2069dffc
MariusM157 <marius.maurer@ifm.com>
2025-03-06 21:01:08 +0100

    Ordner "Meine Website" komplett gelöscht

--- MeineWebseite/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.menu-list a').forEach(item => {
-            item.addEventListener('mouseover', () => {
-                item.style.color = 'blue';
-            });
-            item.addEventListener('mouseout', () => {
-                item.style.color = 'black';
-            });
-        });
