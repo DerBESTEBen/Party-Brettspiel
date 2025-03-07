8d4860b9a09f2ac5b60926f8b5a3040f74a6c8e9
Marius Maurer <marius.maurer@ifm.com>
2025-03-07 10:35:30 +0100

    Aufbau der Ordnerstruktur (#2)
    * Ordner "Meine Website" gelöscht
    
    * Ordner "Meine Website" komplett gelöscht

--- PHP/spieloberflaeche.php
+++ PHP/spieloberflaeche.php
@@ -1,3 +1,7 @@
+<<<<<<< HEAD:PHP/spieloberflaeche.php
+<<<<<<<< HEAD:PHP/spieloberflaeche.php
+=======
+>>>>>>> bceaec6325b52213e3faa19fdc9ecf64e8c97c9a:MeineWebseite/PHP/spieloberflaeche.php
 <?php
 include_once 'spieler.php'; // Spieler-Klasse einbinden
 
@@ -79,3 +83,8 @@ if (isset($_GET['names']) && isset($_GET['players'])) {
 
 </body>
 </html>
+<<<<<<< HEAD:PHP/spieloberflaeche.php
+========
+>>>>>>>> bceaec6325b52213e3faa19fdc9ecf64e8c97c9a:MeineWebseite/PHP/spiel.php
+=======
+>>>>>>> bceaec6325b52213e3faa19fdc9ecf64e8c97c9a:MeineWebseite/PHP/spieloberflaeche.php
