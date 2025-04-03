660ac2e6af98b43b1b9d95b2b36ee5d5555b5d4c
MariusM157 <marius.maurer@ifm.com>
2025-04-03 23:18:51 +0200

    Tests erstellt, testen aber nichts

--- Test_Dateien/spiel.test.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-describe('spiel.php', () => {
-  test('sollte initiale Spieldaten liefern', async () => {
-    const res = await fetch('http://localhost/PartyBrettspiel/PHP/spiel.php');
-    expect(res.status).toBe(200);
-  });
-});
--- Test_Dateien/spieldfelder.test.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-describe('spieldfelder.php', () => {
-  test('sollte das Spielfeld definieren oder rendern', async () => {
-    const res = await fetch('http://localhost/PartyBrettspiel/PHP/spieldfelder.php');
-    expect(res.status).toBe(200);
-  });
-});
